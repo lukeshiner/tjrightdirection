581e4ae9849f23713125c1bf3f7000623accdb53
Luke Shiner <luke@lukeshiner.com>
2017-02-19 18:10:52 +0000

    Adds status messages after contact form is submitted

--- contact/urls.py
+++ contact/urls.py
@@ -6,4 +6,6 @@ app_name = 'contact'
 
 urlpatterns = [
     url(r'^$', views.contact, name='contact'),
+    url(r'^message_success/$', views.success, name='success'),
+    url(r'^message_failure/$', views.failure, name='failure'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,18 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . forms import ContactForm
 
 
 def contact(request):
     form = ContactForm()
-    status = None
     if request.method == 'POST':
         form = ContactForm(data=request.POST)
         if form.is_valid():
             try:
                 form.send_mail()
             except:
-                status = 'failed'
+                return redirect('contact:failure')
             form = ContactForm()
-            status = 'success'
-    return render(request, 'contact/contact.html', {
-        'form': form, 'status': status})
+            return redirect('contact:success')
+    return render(request, 'contact/contact.html', {'form': form})
+
+
+def success(request):
+    form = ContactForm()
+    return render(request, 'contact/success.html', {'form': form})
+
+
+def failure(request):
+    form = ContactForm()
+    return render(request, 'contact/success.html', {'form': form})

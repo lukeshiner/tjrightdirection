1f78ef6c00b71d4a9b8cc69123bfc3c04fbb1675
Luke Shiner <luke@lukeshiner.com>
2018-05-27 15:11:43 +0100

    Update tjrightdirection app.

---
+++ tjrightdirection/__init__.py
@@ -0,0 +1,1 @@
+"""The tjrightdirection site."""
--- tjrightdirection/urls.py
+++ tjrightdirection/urls.py
@@ -1,32 +1,19 @@
-"""tjrightdirection URL Configuration
+"""URLs for tjrightdirection."""
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf import settings
-from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.urls import include, path
 from hitcounter import views as hitcounter_views
 
 urlpatterns = [
-    url(r'^gallery/', include('gallery.urls')),
-    url(r'^recomendations/', include('recomendations.urls')),
-    url(r'^contact/', include('contact.urls')),
-    url(r'^hitcounter/', include('hitcounter.urls')),
-    url(r'^', include('tjhome.urls')),
-    url(r'^admin/hitcounter', hitcounter_views.HitCounter.as_view()),
-    url(r'^admin/', admin.site.urls),
+    path('', include('tjhome.urls')),
+    path('gallery/', include('gallery.urls')),
+    path('recomendations/', include('recomendations.urls')),
+    path('contact/', include('contact.urls')),
+    path('hitcounter/', include('hitcounter.urls')),
+    path('admin/hitcounter', hitcounter_views.HitCounter.as_view()),
+    path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:

f1beae10981c364f9ded30fa7f0d4040fbe765ff
Luke Shiner <luke@lukeshiner.com>
2020-04-04 15:42:15 +0100

    fix: Fix contact form failure message

--- contact/views.py
+++ contact/views.py
@@ -1,26 +1,31 @@
-from django.shortcuts import render, redirect
-from . forms import ContactForm
+"""Views for the contact app."""
+from django.shortcuts import redirect, render
+
+from .forms import ContactForm
 
 
 def contact(request):
+    """View for handling the contact form."""
     form = ContactForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ContactForm(data=request.POST)
         if form.is_valid():
             try:
                 form.send_mail()
-            except:
-                return redirect('contact:failure')
+            except Exception:
+                return redirect("contact:failure")
             form = ContactForm()
-            return redirect('contact:success')
-    return render(request, 'contact/contact.html', {'form': form})
+            return redirect("contact:success")
+    return render(request, "contact/contact.html", {"form": form})
 
 
 def success(request):
+    """View for displaying the contact form success message."""
     form = ContactForm()
-    return render(request, 'contact/success.html', {'form': form})
+    return render(request, "contact/success.html", {"form": form})
 
 
 def failure(request):
+    """View for displaying a contact form error."""
     form = ContactForm()
-    return render(request, 'contact/success.html', {'form': form})
+    return render(request, "contact/failure.html", {"form": form})

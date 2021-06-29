6db024655a7743c5c2eb17140c6b21d159f61deb
Luke Shiner <luke@lukeshiner.com>
2021-06-29 20:39:44 +0100

    Fix incorrect redirect after failed contact submission

--- contact/views.py
+++ contact/views.py
@@ -1,26 +1,32 @@
-from django.shortcuts import render, redirect
-from . forms import ContactForm
+"""Views for the contact app."""
+
+from django.shortcuts import redirect, render
+
+from .forms import ContactForm
 
 
 def contact(request):
+    """View for the contact page."""
     form = ContactForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = ContactForm(data=request.POST)
         if form.is_valid():
             try:
                 form.send_mail()
-            except:
-                return redirect('contact:failure')
+            except Exception:
+                return redirect("contact:failure")
             form = ContactForm()
-            return redirect('contact:success')
-    return render(request, 'contact/contact.html', {'form': form})
+            return redirect("contact:success")
+    return render(request, "contact/contact.html", {"form": form})
 
 
 def success(request):
+    """View for successful messages."""
     form = ContactForm()
-    return render(request, 'contact/success.html', {'form': form})
+    return render(request, "contact/success.html", {"form": form})
 
 
 def failure(request):
+    """View for failed messages."""
     form = ContactForm()
-    return render(request, 'contact/success.html', {'form': form})
+    return render(request, "contact/failure.html", {"form": form})
